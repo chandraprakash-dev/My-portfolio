b9c3c6b9a5d2820a96b9a1d17c658621e37b15ae
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-12-01 14:20:04 +0530

    Update basic styling

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,8 +4,8 @@ function ProjectCard(props) {
     return(
         <div className="project-card">
             <p>{props.values.name}</p>
-            <a href={props.values.code}><i className="fas fa-laptop-code"></i></a>
-            <a href={props.values.live}><i className="fab fa-safari"></i></a>
+            <a target='_blank' href={props.values.code}><i className="fas fa-laptop-code"></i></a>
+            <a target='_blank' href={props.values.live}><i className="fab fa-safari"></i></a>
         </div>
     )
 }
