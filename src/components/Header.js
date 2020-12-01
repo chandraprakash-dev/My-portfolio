899980416f24c4cda744b7c304c63fee4e48483c
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-12-01 16:29:19 +0530

    show projects in a minimalistic grid

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,13 @@ function Header() {
             <h3>Front end developer</h3>
             <p>Bangalore, India</p>
             <div className="social">
-                <i className="fab fa-github"></i>
-                <i className="fab fa-linkedin-in"></i>
-                <i className="far fa-envelope"></i>
+                <a target='_blank' href="https://github.com/chandraprakash-dev"><i className="fab fa-github fa-lg"></i></a>
+                <a target='_blank' href="https://www.linkedin.com/in/chandraprakash30/"><i className="fab fa-linkedin-in fa-lg"></i></a>
+                {/*<a target='_blank' href=""><i className="far fa-envelope fa-lg"></i></a>*/}
             </div>
-            <div>
-                Resume
-            </div>
+            {/*<div>*/}
+            {/*    Resume*/}
+            {/*</div>*/}
         </header>
     )
 }
