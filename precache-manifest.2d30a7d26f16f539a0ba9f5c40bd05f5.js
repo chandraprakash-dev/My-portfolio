d3fc79df8f9a6a20303adf187e5649a063a53254
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-12-01 14:14:09 +0530

    Updates

---
+++ precache-manifest.2d30a7d26f16f539a0ba9f5c40bd05f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa373e40063e51dde2b5473cf6e1af7",
+    "url": "/My-portfolio/index.html"
+  },
+  {
+    "revision": "7357af019cfbac6eb3cf",
+    "url": "/My-portfolio/static/js/2.9e7789ca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/My-portfolio/static/js/2.9e7789ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23519dbf74eb79ba454",
+    "url": "/My-portfolio/static/js/main.ed7a9299.chunk.js"
+  },
+  {
+    "revision": "957ead9782c22a9bc9c8",
+    "url": "/My-portfolio/static/js/runtime-main.5f6c66f6.js"
+  }
+]);
